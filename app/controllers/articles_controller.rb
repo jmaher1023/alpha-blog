452d6d8f484c89de3e58ed89095e000f4ef92496
Jim Maher <jim@1023designstudio.com>
2015-12-31 08:07:37 +0000

    Added admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,7 +52,7 @@ class ArticlesController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You do not have the permissions to edit or delete this article"
         redirect_to root_path
       end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151230035005) do
+ActiveRecord::Schema.define(version: 20151231055216) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20151230035005) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
+    t.string  "username"
+    t.string  "email"
+    t.string  "password_digest"
+    t.boolean "admin",           default: false
   end
 
 end
